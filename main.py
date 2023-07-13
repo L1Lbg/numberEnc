3c39c7a4b8af23a19eee6f25b210d77afc290c4c
galaxygame83 <23119905-galaxygame83@users.noreply.replit.com>
2023-07-13 13:11:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import secrets
+import string  
+
+def GenerateNumbers(max):  
+  numbers = [1,5]
+  num_gen = 1
+  # ADD 1,5 AND 10eN NUMBERS TO PREVENT TOO LONG KEYS
+  while num_gen < max:
+    num_gen = num_gen * 10
+    if num_gen < max:
+      numbers.append(num_gen)
+  # APPEND OTHER RANDOM NUMBERS
+  # IF MAX IS BIGGER THAN 10, CREATE A LIST WITH 10 NUMBERS
+  if max > 10:
+    for i in range(10): 
+      rand = secrets.randbelow(max)
+      if rand not in numbers and rand != 0:
+        numbers.append(rand)
+  else:
+    raise Exception('Number has to be > 10')
+  return numbers
+
+#PICK NUMBER WHILE MAKING SURE THEY ARE DIFFERENT
+def PickNumber(numbers,pattern):
+  #UPDATE USED_NUMBERS
+  used_numbers = []
+  if len(list(pattern.items())) != 0:
+    for i in range(len(list(pattern.items()))):
+      used_numbers.append(list(pattern.items())[i][1])  
+  
+  pick = str(numbers[secrets.randbelow(len(numbers))])
+  while pick in used_numbers:
+    pick = str(numbers[secrets.randbelow(len(numbers))])
+  return int(pick)
+
+#CREATE THE DICT THAT ASSOCIATES LETTERS TO NUMBERS
+def CreatePattern(numbers):
+  alphabet = list(string.ascii_letters)
+  pattern = {}
+  for i in range(len(numbers)):
+    pattern[alphabet[i]] = PickNumber(numbers, pattern)
+
+  #SORT PATTERN IN KEY VALUE BIG TO SMALL
+  pattern = dict(sorted(pattern.items(), key=lambda x :int(x[1]), reverse=True))
+  return pattern
+
+def GenerateKey(num, pattern):
+  key = str()
+  current_num = 0 #WHAT THE KEY BEING GENERATED CURRENTLY IS
+  while current_num != num:
+    for i in range(len(pattern.items())):
+      #REUSE THE BIGGEST POSSIBLE NUMBER IN THE DICT, TO MAKE SURE KEY ISN'T LONG
+      while num - current_num >= list(pattern.items())[i][1]:
+        key += list(pattern.items())[i][0]
+        current_num = current_num + list(pattern.items())[i][1]
+  return str(key)
+  
+def DecryptKey(key, pattern):
+  num = 0
+  for i in key:
+    num += int(pattern[i])
+  return num
---
+++ test.py
@@ -0,0 +1,20 @@
+from main import *
+import pprint
+#TEST THE PROGRAM
+to_encrypt = int(input('Input number to encrypt: \n'))
+
+#GENERATE A LIST OF RANDOM NUMBERS WITH A MAX VALUE (RECOMMENDED 2 TIMES SMALLER THAN THE ENCRIPTED NUM)
+numbers = GenerateNumbers(int(to_encrypt / 2))
+print(numbers)
+
+#ASSOCIATE NUMBERS TO RANDOM LETTERS AND CREATE PATTERN
+pattern = CreatePattern(numbers)
+pprint.pprint(pattern, sort_dicts=False)
+
+#GENERATE KEY THAT EQUALS TO WANTED NUM WITH PATTERN
+key = GenerateKey(to_encrypt, pattern)
+print(f'Generated key : {key}')
+
+#DECRYPT KEY WITH PATTERN
+num = DecryptKey(key, pattern)
+print(f'Decrypted num : {num}')
